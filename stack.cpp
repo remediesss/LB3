b124a0292852da756e58c5a27f88f0eca3e7bfbb
remediesss <167404465+remediesss@users.noreply.github.com>
2024-09-24 08:21:01 +0300

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,238 @@
+
+#include <iostream>
+#include <cstring>
+#include <limits>
+
+struct node
+{
+	char inf[256];
+	struct node* next;
+};
+
+
+struct node* head = NULL, * last = NULL, * f = NULL; // указатели на первый и последний элементы списка
+int dlinna = 0;
+
+void spstore(void), review(void), del(char* name);
+
+char find_el[256];
+struct node* find(char* name); // функция нахождения элемента
+struct node* get_struct(void); // функция создания элемента
+
+
+
+struct node* get_struct(void)
+{
+	struct node* p = NULL;
+	char s[256];
+
+	if ((p = (node*)malloc(sizeof(struct node))) == NULL)  // выделяем память под новый элемент списка
+	{
+		printf("Ошибка при распределении памяти\n");
+		exit(1);
+	}
+
+	printf("Введите название объекта: \n");   // вводим данные
+	scanf("%s", s);
+	if (*s == 0)
+	{
+		printf("Запись не была произведена\n");
+		return NULL;
+	}
+	strcpy(p->inf, s);
+
+	p->next = NULL;
+
+	return p;		// возвращаем указатель на созданный элемент
+}
+
+/* Последовательное добавление в список элемента (в конец)*/
+void spstore(void)
+{
+	struct node* p = NULL;
+	p = get_struct();
+	if (head == NULL && p != NULL)	// если списка нет, то устанавливаем голову списка
+	{
+		head = p;
+		last = p;
+	}
+	else if (head != NULL && p != NULL) // список уже есть, то вставляем в конец
+	{
+		last->next = p;
+		last = p;
+	}
+	return;
+}
+
+
+/* Просмотр содержимого списка. */
+void review(void)
+{
+	struct node* struc = head;
+	if (head == NULL)
+	{
+		printf("Список пуст\n");
+	}
+	while (struc)
+	{
+		printf("Имя - %s, \n", struc->inf);
+		struc = struc->next;
+	}
+	return;
+}
+
+
+
+
+/* Поиск элемента по содержимому. */
+struct node* find(char* name)
+{
+	struct node* struc = head;
+	if (head == NULL)
+	{
+		printf("Список пуст\n");
+	}
+	while (struc)
+	{
+		if (strcmp(name, struc->inf) == 0)
+		{
+			return struc;
+		}
+		struc = struc->next;
+	}
+	printf("Элемент не найден\n");
+	return NULL;
+}
+
+/* Удаление элемента по содержимому. */
+void del(char* name)
+{
+	struct node* struc = head; // указатель, проходящий по списку установлен на начало списка
+	struct node* prev;// указатель на предшествующий удаляемому элемент
+	int flag = 0;      // индикатор отсутствия удаляемого элемента в списке
+
+	if (head == NULL) // если голова списка равна NULL, то список пуст
+	{
+		printf("Список пуст\n");
+		return;
+	}
+
+	if (strcmp(name, struc->inf) == 0) // если удаляемый элемент - первый
+	{
+		flag = 1;
+		head = struc->next; // установливаем голову на следующий элемент
+		free(struc);  // удаляем первый элемент
+		struc = head; // устанавливаем указатель для продолжения поиска
+	}
+	else
+	{
+		prev = struc;
+		struc = struc->next;
+	}
+
+	while (struc) // проход по списку и поиск удаляемого элемента
+	{
+		if (strcmp(name, struc->inf) == 0) // если нашли, то
+		{
+			flag = 1;         // выставляем индикатор
+			if (struc->next)  // если найденный элемент не последний в списке
+			{
+				prev->next = struc->next; // меняем указатели
+				free(struc);		    // удаляем  элемент
+				struc = prev->next; // устанавливаем указатель для продолжения поиска
+			}
+			else			// если найденный элемент последний в списке
+			{
+				prev->next = NULL; // обнуляем указатель предшествующего элемента
+				free(struc);	// удаляем  элемент
+				return;
+			}
+		}
+		else // если не нашли, то
+		{
+			prev = struc; // устанавливаем указатели для продолжения поиска
+			struc = struc->next;
+		}
+	}
+
+	if (flag == 0)				// если флаг = 0, значит нужный элемент не найден
+	{
+		printf("Элемент не найден\n");
+		return;
+	}
+}
+
+
+struct node* pop() {
+	if (!head) {
+		return nullptr;
+	}
+
+	if (!head->next) {
+		node* temp = head;
+		head = nullptr;
+		return temp;
+	}
+
+	node* temp = head;
+	while (temp->next->next != nullptr) {
+		temp = temp->next;
+	}
+
+	node* last = temp->next;
+	temp->next = nullptr;
+	return last;
+}
+
+void stack()
+{
+	while (true)
+	{
+		std::cout << "options: \n";
+		std::cout << "push: 1\n";
+		std::cout << "pop: 2\n";
+		std::cout << "exit: 3\n";
+		std::cout << "your choice: ";
+
+		int choice;
+		std::cin >> choice;
+
+		std::cout << std::endl;
+
+		if (choice == 1)
+		{
+			spstore();
+		}
+		else if (choice == 2)
+		{
+			struct node* popped = pop();
+			if (popped == nullptr)
+			{
+				std::cout << "Stack is Empty\n\n";
+			}
+			else
+			{
+				std::cout << "Last block deleted. Name block - " << popped->inf << std::endl;
+			}
+		}
+
+		else if (choice == 3)
+		{
+			return;
+		}
+		else
+		{
+			std::cout << "Wrong operation\n\n";
+		}
+
+	}
+
+}
+
+
+
+int main()
+{
+	stack();
+	exit(0);
+}
